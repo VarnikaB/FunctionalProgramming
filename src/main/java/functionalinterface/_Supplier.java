ad3c86abd7febf9743926017fa4d627bb2012679
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-09-17 10:34:05 +0530

    [Varnika] Add. get for sout using supplier

--- src/main/java/functionalinterface/_Supplier.java
+++ src/main/java/functionalinterface/_Supplier.java
@@ -1,14 +1,15 @@
 package functionalinterface;
 
+import java.util.function.Supplier;
+
 public class _Supplier {
+    static Supplier<String> getDBConnectionUrlSupplier = () ->
+            "jdbc://localhost:5432/users";
+
     public static void main(String[] args) {
 
-        System.out.println(getDBConnectionURL());
+        System.out.println(getDBConnectionUrlSupplier.get());
 
-    }
-
-    private static String getDBConnectionURL() {
-        return "jdbc://localhost:5432/users";
     }
 
 
