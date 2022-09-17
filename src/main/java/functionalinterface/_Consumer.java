f5d0dc09fdc5bc8bfda573a9c18828f5aa53b5ee
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-09-17 10:21:44 +0530

    [Varnika] Add. use biconsumer

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -7,12 +8,20 @@ public class _Consumer {
             System.out.println("Hello " + customer.name
                     + ", thanks for registering for phone number "
                     + customer.phoneNumber);
+    static BiConsumer<Customer, Boolean> greetConsumerCustomerShowHidePhoneNumber
+            = (customer, showPhoneNumber) -> {
+        System.out.println("Hello " + customer.name +
+                (showPhoneNumber ? (", thanks for registering for phone number "
+                        + customer.phoneNumber) : ""));
+    };
 
     public static void main(String[] args) {
 
         Customer customer = new Customer("Varnika", "1234567890");
-
         greetConsumerCustomer.accept(customer);
+
+        greetConsumerCustomerShowHidePhoneNumber.accept(customer, false);
+        greetConsumerCustomerShowHidePhoneNumber.accept(customer, true);
 
     }
 
