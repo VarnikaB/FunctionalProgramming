9b222cb91ff186da0ae0dcef29a3a6a42f89e532
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-09-17 10:27:51 +0530

    [Varnika] Add. test for phone number using predicate

--- src/main/java/functionalinterface/_Predicate.java
+++ src/main/java/functionalinterface/_Predicate.java
@@ -3,11 +3,14 @@ package functionalinterface;
 import java.util.function.Predicate;
 
 public class _Predicate {
+    static Predicate<String> isValidPhoneNumber = phoneNumber ->
+            phoneNumber.startsWith("1") && phoneNumber.length() == 10;
 
     public static void main(String[] args) {
 
-        System.out.println(isPhoneNumberValid("1234567890"));
-        System.out.println(isPhoneNumberValid("0123456789"));
+        System.out.println(isValidPhoneNumber.test("1234567890"));
+        System.out.println(isValidPhoneNumber.test("0123456789"));
+        System.out.println(isValidPhoneNumber.test("01234567890"));
 
 
     }
