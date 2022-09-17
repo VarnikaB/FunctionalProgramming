32cf95d15880ac5d20922d09d465acb6fd856d7f
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-09-17 09:57:23 +0530

    [Varnika] Add. use 2 functional interfaces and variables to get result

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -6,10 +6,15 @@ public class _Function {
 
     //Function<Input, Output>
     static Function<Integer, Integer> incrementByOne = number -> ++number;
+    static Function<Integer, Integer> multiplyByTen = number -> number * 10;
 
     public static void main(String[] args) {
-        Integer increment = incrementByOne.apply(0);
+
+        Integer increment = incrementByOne.apply(1);
         System.out.println(increment);
+
+        Integer product = multiplyByTen.apply(increment);
+        System.out.println(product);
 
     }
 
