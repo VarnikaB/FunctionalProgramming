1639869c92f8df8af5b6fb5ff93d2a4d32f1d8b7
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-09-17 10:07:24 +0530

    [Varnika] Add. use of bifunction

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -7,15 +8,15 @@ public class _Function {
     //Function<Input, Output>
     static Function<Integer, Integer> incrementByOne = number -> ++number;
     static Function<Integer, Integer> multiplyByTen = number -> number * 10;
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBy =
+            (numberToIncrementByOne, numberToMultiplyBy) ->
+                    (++numberToIncrementByOne) * numberToMultiplyBy;
 
     public static void main(String[] args) {
 
-        Function<Integer, Integer> incrementByOneAndThenMultiplyByTen
-                = incrementByOne.andThen(multiplyByTen);
-
-        Integer result = incrementByOneAndThenMultiplyByTen.apply(1);
+        //Bifunction takes 2 parameters
+        Integer result = incrementByOneAndMultiplyBy.apply(2, 4);
         System.out.println(result);
-
     }
 
 }
