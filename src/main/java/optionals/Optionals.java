e9ecace880c23f65ade8d31d14ce488bc112b0f5
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-09-17 11:00:25 +0530

    [Varnika] Add. optional ifPresentElse

--- src/main/java/optionals/Optionals.java
+++ src/main/java/optionals/Optionals.java
@@ -4,11 +4,9 @@ import java.util.Optional;
 
 public class Optionals {
     public static void main(String[] args) {
-        Object value = Optional.ofNullable(null)
-                .orElseGet(() -> "Default Value");
-
-        System.out.println(value);
-
-
+        Optional.ofNullable("Hello")
+                .ifPresentOrElse(System.out::println, ()-> {
+                    System.out.println("Something is wrong");
+                });
     }
 }
